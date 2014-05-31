3dfa388ee8b65d01ad64d914b380470a7f3cba05
Shahn-Auronas <shahn.auronas@gmail.com>
2014-05-31 16:36:38 -0700

    modified generator functions to use chained promises

--- b4/lib/bundle.js
+++ b4/lib/bundle.js
@@ -43,121 +43,129 @@ module.exports = function (config, app) {
 		//returning a promise that is fulfilled if the func calls back
 		//with a result or rejects if error
 		Q.nfcall(request.get, config.b4db + '/' + req.params.id)
-		  .then(function (args) {
-			let couchRes = args[0],
-			    bundle = JSON.parse(args[1]);
-			res.json(couchRes.statusCode, bundle);
-		}, function (err) {
-			res.json(502, {
-				error: 'bad_gateway',
-				reason: err.code
-			});
-		}).done();
+		    .then(function (args) {
+			    let couchRes = args[0],
+			        bundle = JSON.parse(args[1]);
+			    res.json(couchRes.statusCode, bundle);
+		    }, function (err) {
+			    res.json(502, {
+				    error: "bad_gateway",
+				    reason: err.code
+			    });
+		    })
+		    .done();
 	});
 	//Set the specified bundle's name with the specified name
 	//curl -X PUT http://localhost:3000/api/bundle/<id>/name/<name>
 	app.put('/api/bundle/:id/name/:name', function (req, res) {
 		Q.nfcall(request.get, config.b4db + '/' + req.params.id)
-		  .then(function (args) {
-		  	let couchRes = args[0],
-		  	    bundle = JSON.parse(args[1]);
-		  	if (couchRes.statusCode !== 200) {
-		  		return [couchRes, bundle];
-		  	}
-		  	bundle.name = req.params.name;
-		  	return Q.nfcall(reques.put, {
-                url: config.b4db + '/' + req.params.id,
-                json: bundle
-		  	});
-		  }).then(function (args) {
-		  	let couchRes = args[0],
-		  	    body = args[1];
-		  	res.json(couchRes.statusCode, body);
-		  }).catch(function (err) {
-		  	res.json(502, {
-		  		error: 'bad_gateway',
-		  		reason: err.code
-		  	});
-		  }).done();
+		    .then(function (args) {
+		  	    let couchRes = args[0],
+		  	        bundle = JSON.parse(args[1]);
+		  	    if (couchRes.statusCode !== 200) {
+		  		    return [couchRes, bundle];
+		  	    }
+		  	    bundle.name = req.params.name;
+		  	    return Q.nfcall(reques.put, {
+                    url: config.b4db + '/' + req.params.id,
+                    json: bundle
+		  	    });
+		    })
+		    .then(function (args) {
+		  	    let couchRes = args[0],
+		  	        body = args[1];
+		  	    res.json(couchRes.statusCode, body);
+		    })
+		    .catch(function (err) {
+		  	    res.json(502, {
+		  		    error: "bad_gateway",
+		  		    reason: err.code
+		  	    });
+		    })
+		    .done();
 	});
 	//put a book into a bundle by its id
 	//curl  -X PUT http://localhost:3000/api/bundle/<id>/book/<pgid>
 	app.put('/api/bundle/:id/book/:pgid', function (req, res) {
-		//denodeify: creates a promise-returning function from
-		//a node.js style function, binding it with the variadic args
-		let get = Q.denodeify(request.get),
-		    put = Q.denodeify(reques.put);
-		//generator function is converted into a deffered function
-		//to reduce nested callbacks
-		Q.async(function* () {
-			let args,
-			    couchRes,
-			    bundle,
-			    book;
-			//grab the bundle from the b4 database
-			args = yield get(config.b4db + req.params.id);
-			couchRes = args[0];
-			bundle = JSON.parse(args[1]);
-			//fail fast if we cannot retrieve the bundle
-			if (couchRes.statusCode !== 200) {
-				res.json(couchRes.statusCode, bundle);
-				return;
-			}
-			//look up the book by its Project Gutenberge ID
-			args = yield get(config.bookdb + req.params.pgid);
-			couchRes = args[0];
-			book = JSON.parse(args[1]);
-			//fail fast if we cannot retrieve the book
-			if (couchRes.statusCode !== 200) {
-				res.json(couchRes.statusCode, book);
-				return;
-			}
-			//add the book to the bundle and put it back in CouchDB
-			bundle.books[book._id] = book.title;
-			args = yield put({
-				url: config.b4db + bundle._id,
-				json: bundle
-			});
-			res.json(args[0].statusCode, args[1]);
-		})().catch(function (err) {
-			res.json(502, {
-				error: 'bad_gateway',
-				reason: err.code
-			});
-		});
+		let args,
+		    couchRes,
+		    bundle,
+		    book;
+		//grab the bundle from the b4 database
+		Q.nfcall(request.get, config.b4db + '/' + req.params.id)
+		    .then(function (args) {
+		  	    couchRes = args[0];
+		  	    bundle = JSON.parse(args[1]);
+		  	    //fail fast if we can't retrieve the bundle
+		  	    if (couchRes.statusCode !== 200) {
+		  		    return [couchRes, bundle];
+		  	    }
+		  	    //look up the book by its Project Gutenberg ID
+		  	    return Q.nfcall(reques.get, config.bookdb + '/' + req.params.pgid);
+		    })
+		    .then(function (args) {
+		  	    couchRes = args[0];
+		  	    book = JSON.parse(args[1]);
+		  	    //fail fast if we can't retrieve the book
+		  	    if (couchRes.statusCode !== 200) {
+		  		    return [couchRes, book];
+		  	    }
+		  	    bundle.books[book._id] = book.title;
+		  	    //add the book to the bundle and put it back in CouchDB
+		  	    return Q.nfcall(request.put, {
+		  		    url: config.b4db + '/' + bundle._id,
+		  		    json: bundle
+		  	    });
+		    })
+		    .then(function (args) {
+		  	    res.json(args[0].statusCode, args[1]);
+		    })
+		    .catch(function (err) {
+		  	    res.json(502, {
+		  		    error: "bad_gateway",
+		  		    reason: err.code
+		  	    });
+		    })
+		    .done();
 	});
     //Remove a book from a bundle
     //curl -X DELETE http://localhost:3000/api/bundle/<id>/book/<pgid>
     app.del('/api/bundle/:id/book/:pgid', function (req, res) {
-    	Q.async(function* () {
-    		let args = yield Q.nfcall(request, config.b4db + req.params.id),
-    		    couchRes = args[0],
-    		    bundle = JSON.parse(args[1]);
-    		//fail fast if we don't retrieve the bundle
-    		if (couchRes.statusCode !== 200) {
-    			res.json(couchRes.statusCode, bundle);
-    			return;
-    		}
-    		//fail if the bundle doesn't already have that book
-            if (!(req.params.pgid in bundle.books)) {
-            	res.json(409, {
-            		error: 'conflict',
-            		reason: 'Bundle does not contain that book.'
-            	});
-            	return;
-            }
-            //remove the book from the bundle
-            delete bundle.books[req.params.pgid];
-            //put the updated bundle in CouchDB
-            request.put({
-            	url: config.b4db + bundle._id,
-            	json: bundle
-            }).pipe(res);
-    	})().catch(function (err) {
-    		res.json(502, {
-    			error: 'bad_gateway',
-    			reason: err.code
-    		});
-    	});
+    	let args,
+    	    couchRes,
+    	    bundle;
+    	Q.nfcall(request.get, config.b4db + '/' + req.params.id)
+    	    .then(
+    	    	function (args) {
+                    couchRes = args[0],
+                    bundle = JSON.parse(args[1]);
+                    //fail fast if we can't retrieve the bundle
+                    if (couchRes.statusCode !== 200) {
+                    	res.json(couchRes.statusCode, bundle);
+                    	return;
+                    }     
+                    //fail fast if bundle doesn't already have that book
+                    if (!(req.params.pgid in bundle.books)) {
+                    	res.json(409, {
+                    		error: "conflict",
+                    		reason: "Bundle does not contain that book."
+                    	});
+                    	return;
+                    }
+                    //remove the book from the bundle
+                    delete bundle.books[req.params.pgid];
+                    //put the updated bundle back in CouchDB
+                    request.put({
+                    	url: config.b4db + bundle._id,
+                    	json: bundle
+                    })
+                    .pipe(res);
+    		    })
+                .catch(function (err) {
+                	res.json(502, {
+                		error: "bad_gateway",
+                		reason: err.code
+                	});
+                });
     });
 };
